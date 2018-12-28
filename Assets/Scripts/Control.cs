1956a5796a34f642897fba875d8953e9a233bb0b
Ben Hockley <ben.hockley@readify.net>
2018-12-28 20:18:18 +1000

    Started moving MoveDisk into own class to seperate the input logic

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -7,48 +7,15 @@ public class Control : MonoBehaviour
 
     void Start()
     {
-        _moveDisk = new GameObject("MoveDisk");
-        _planeCursorTop = new GameObject("PlaneCursor");
-        _planeCursorTop.DrawCircle(1, 0.1f, UiColor);
-        _planeCursorTop.transform.SetParent(_moveDisk.transform);
-
-        _planeCursorBottom = new GameObject("PlaneCursor");
-        _planeCursorBottom.DrawCircle(1, 0.1f, UiColor);
-        _planeCursorBottom.transform.SetParent(_moveDisk.transform);
-
-        _vertConnectingLine = new GameObject("ConnectingLine");
-        _vertConnectingLine.transform.SetParent(_moveDisk.transform);
-        var lineRenderer = _vertConnectingLine.AddComponent<LineRenderer>();
-        lineRenderer.startWidth = 0.1f;
-        lineRenderer.endWidth = 0.1f;
-        lineRenderer.enabled = true;
-        lineRenderer.material.color = UiColor;
-
-        _moveDisk.SetActive(false);
+        var moveDiskGameObject = new GameObject("MoveDisk");
+        _moveDisk = moveDiskGameObject.AddComponent<MoveDisk>();
+        _moveDisk.enabled = true;
     }
 
-    private GameObject _moveDisk;
-    private GameObject _planeCursorTop;
-    private GameObject _planeCursorBottom;
-    private GameObject _vertConnectingLine;
+    private MoveDisk _moveDisk;
+
 
     private bool _active;
-    private Vector3 _planeHitPoint;
-    private Vector3 HitPoint => _planeHitPoint + new Vector3(0, _verticalOffset, 0);
-    private float _verticalOffset;
-
-    private float _mouseButtonDownTime;
-    private float _mouseButtonDebounceInterval = 0.1f;
-
-    private void StartDebounce()
-    {
-        _mouseButtonDownTime = Time.time;
-    }
-
-    private bool HasDebounceCompleted()
-    {
-        return Time.time - _mouseButtonDownTime > _mouseButtonDebounceInterval;
-    }
 
     void Update()
     {
@@ -60,93 +27,33 @@ public class Control : MonoBehaviour
 
         var mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 
+        var movePlane = new Plane(Vector3.up, selectedShip.transform.position);
+
+        if (_active && _moveDisk.LocationSet)
+        {
+            selectedShip.MoveTo(_moveDisk.Location);
+            _active = false;
+            _moveDisk.Deactivate();
+        }
+
+
         if (Input.GetMouseButtonUp(1))
         {
             // Checks that the movement plane for the ship is in view
-            var movePlane = new Plane(Vector3.up, selectedShip.transform.position);
             if (movePlane.Raycast(mouseRay, out _))
             {
-                if (_active)
+                if (!_active)
                 {
-                    DeactivateMoveDisk();
+                    _active = true;
+                    _moveDisk.Activate(selectedShip.gameObject);
                 }
                 else
                 {
-                    ActivateMoveDisc();
+                    _active = false;
+                    _moveDisk.Deactivate();
                 }
             }
         }
-
-        if (!_active) return;
-        
-        if (Input.GetMouseButtonUp(0))
-        {
-            selectedShip.MoveTo(HitPoint);
-            DeactivateMoveDisk();
-            return;
-        }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            StartDebounce();
-        }
-
-        _moveDisk.transform.SetParent(selectedShip.transform);
-        _moveDisk.transform.localPosition = Vector3.zero;
-        
-
-        if ((Input.GetKey(KeyCode.LeftShift) || Input.GetMouseButton(0)) && Camera.main != null 
-                                                                         && HasDebounceCompleted())
-        {
-            var verticalPlane = new Plane(Camera.main.transform.forward, _planeHitPoint);
-
-            if (verticalPlane.Raycast(mouseRay, out var enter))
-            {
-                var raisedPoint = mouseRay.GetPoint(enter);
-                _verticalOffset = raisedPoint.y - _planeHitPoint.y;
-            }
-        }
-        else
-        {
-            var movePlane = new Plane(Vector3.up, selectedShip.transform.position);
-
-            if (movePlane.Raycast(mouseRay, out var enter))
-            {
-                _planeHitPoint = mouseRay.GetPoint(enter);
-
-                var distance = Vector3.Distance(selectedShip.transform.position, _planeHitPoint);
-                
-                _moveDisk.transform.parent = selectedShip.transform;
-                _moveDisk.transform.rotation = Quaternion.AngleAxis(0, Vector3.forward);
-                _moveDisk.transform.localPosition = Vector3.zero;
-                _moveDisk.DrawCircle(distance, 0.1f, UiColor);
-            }
-        }
-
-        UpdateMoveDiscDisplay();
-    }
-
-    private void UpdateMoveDiscDisplay()
-    {
-        _planeCursorTop.transform.position = HitPoint;
-        _planeCursorBottom.transform.position = _planeHitPoint;
-        var line = _vertConnectingLine.GetComponent<LineRenderer>();
-        line.positionCount = 4;
-        line.SetPositions(new[] { _planeHitPoint, HitPoint, _moveDisk.transform.position, _planeHitPoint });
-    }
-
-    private void DeactivateMoveDisk()
-    {
-        _verticalOffset = 0;
-        _moveDisk.SetActive(false);
-        _active = false;
-    }
-
-    private void ActivateMoveDisc()
-    {
-        _verticalOffset = 0;
-        _moveDisk.SetActive(true);
-        _active = true;
     }
 
     public void OnDrawGizmos()
---
+++ Assets/Scripts/MoveDisk.cs
@@ -0,0 +1,129 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+
+public class MoveDisk : MonoBehaviour
+{
+    public Color UiColor = Color.green;
+
+    void Start()
+    {
+        //_moveDisk = new GameObject("MoveDisk");
+        _planeCursorTop = new GameObject("PlaneCursor");
+        _planeCursorTop.DrawCircle(1, 0.1f, UiColor);
+        _planeCursorTop.transform.SetParent(transform);
+
+        _planeCursorBottom = new GameObject("PlaneCursor");
+        _planeCursorBottom.DrawCircle(1, 0.1f, UiColor);
+        _planeCursorBottom.transform.SetParent(transform);
+
+        _vertConnectingLine = new GameObject("ConnectingLine");
+        _vertConnectingLine.transform.SetParent(transform);
+        var lineRenderer = _vertConnectingLine.AddComponent<LineRenderer>();
+        lineRenderer.startWidth = 0.1f;
+        lineRenderer.endWidth = 0.1f;
+        lineRenderer.enabled = true;
+        lineRenderer.material.color = UiColor;
+
+        //this.gameObject.SetActive(false);
+    }
+
+    //private GameObject _moveDisk;
+    private GameObject _planeCursorTop;
+    private GameObject _planeCursorBottom;
+    private GameObject _vertConnectingLine;
+
+    private Vector3 _planeHitPoint;
+    private Vector3 HitPoint => _planeHitPoint + new Vector3(0, _verticalOffset, 0);
+    private float _verticalOffset;
+
+    private float _mouseButtonDownTime;
+    private float _mouseButtonDebounceInterval = 0.1f;
+
+    private void StartDebounce()
+    {
+        _mouseButtonDownTime = Time.time;
+    }
+
+    private bool HasDebounceCompleted()
+    {
+        return Time.time - _mouseButtonDownTime > _mouseButtonDebounceInterval;
+    }
+
+    private GameObject _anchorObject;
+    public void Activate(GameObject targetObject)
+    {
+        _anchorObject = targetObject;
+        LocationSet = false;
+        _verticalOffset = 0;
+        gameObject.SetActive(true);
+    }
+
+    public bool LocationSet { get; private set; }
+    public Vector3 Location { get; private set; }
+
+    public void Deactivate()
+    {
+        LocationSet = false;
+        _verticalOffset = 0;
+        gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (_anchorObject == null) return;
+        var mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        var movePlane = new Plane(Vector3.up, _anchorObject.transform.position);
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Location = HitPoint;
+            LocationSet = true;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartDebounce();
+        }
+
+        transform.SetParent(_anchorObject.transform);
+        transform.localPosition = Vector3.zero;
+
+        if ((Input.GetKey(KeyCode.LeftShift) || Input.GetMouseButton(0)) && Camera.main != null
+                                                                         && HasDebounceCompleted())
+        {
+            var verticalPlane = new Plane(Camera.main.transform.forward, _planeHitPoint);
+
+            if (verticalPlane.Raycast(mouseRay, out var enter))
+            {
+                var raisedPoint = mouseRay.GetPoint(enter);
+                _verticalOffset = raisedPoint.y - _planeHitPoint.y;
+            }
+        }
+        else
+        {
+            if (movePlane.Raycast(mouseRay, out var enter))
+            {
+                _planeHitPoint = mouseRay.GetPoint(enter);
+
+                var distance = Vector3.Distance(_anchorObject.transform.position, _planeHitPoint);
+
+                transform.parent = _anchorObject.transform;
+                transform.rotation = Quaternion.AngleAxis(0, Vector3.forward);
+                transform.localPosition = Vector3.zero;
+                gameObject.DrawCircle(distance, 0.1f, UiColor);
+            }
+        }
+
+        UpdateMoveDiscDisplay();
+    }
+
+    private void UpdateMoveDiscDisplay()
+    {
+        _planeCursorTop.transform.position = HitPoint;
+        _planeCursorBottom.transform.position = _planeHitPoint;
+        var line = _vertConnectingLine.GetComponent<LineRenderer>();
+        line.positionCount = 4;
+        line.SetPositions(new[] { _planeHitPoint, HitPoint, transform.position, _planeHitPoint });
+    }
+}
